629a18465d0d6f3948f1858e2d61628abd87279c
Lucky Tsuma <tsumahmumba@gmail.com>
2020-11-02 16:40:21 +0300

    First Commit

---
+++ app/src/main/java/com/lucky/fundiapp/Employer_Signup.kt
@@ -0,0 +1,13 @@
+package com.lucky.fundiapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Employer_Signup : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_employer__signup)
+    }
+}
---
+++ app/src/main/java/com/lucky/fundiapp/Worker_Signup2.kt
@@ -0,0 +1,12 @@
+package com.lucky.fundiapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Worker_Signup2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_worker__signup2)
+    }
+}
