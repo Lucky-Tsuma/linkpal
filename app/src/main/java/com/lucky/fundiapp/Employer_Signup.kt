5a5a016a58bfa79346e2493416b578659755475b
Lucky Tsuma <tsumahmumba@gmail.com>
2020-11-13 22:47:22 +0300

    Misc

--- app/src/main/java/com/lucky/fundiapp/Employer_Signup.kt
+++ app/src/main/java/com/lucky/fundiapp/Employer_Signup.kt
@@ -1,11 +1,10 @@
 package com.lucky.fundiapp
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Employer_Signup : AppCompatActivity() {
 
+class Employer_Signup : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_employer__signup)
