3204c6a2117d0ad078272156548eeb5d0d388716
Lucky Tsuma <tsumahmumba@gmail.com>
2020-11-13 22:36:43 +0300

    Created intents

--- app/src/main/java/com/lucky/fundiapp/MainActivity.kt
+++ app/src/main/java/com/lucky/fundiapp/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.lucky.fundiapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_log_in.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+
+        button_create_account.setOnClickListener {
+            val intent = Intent(this, Signup::class.java)
+            startActivity(intent)
+        }
+        
+        about.setOnClickListener {
+            val intent = Intent(this, About::class.java)
+            startActivity(intent)
+        }
     }
 }
