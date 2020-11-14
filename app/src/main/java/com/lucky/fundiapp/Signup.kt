1278ddea82b30c9c3164b3c614913cb06bea2bd9
Lucky Tsuma <tsumahmumba@gmail.com>
2020-11-14 21:43:47 +0300

    Made intents to start other activities as per the buttons

--- app/src/main/java/com/lucky/fundiapp/Signup.kt
+++ app/src/main/java/com/lucky/fundiapp/Signup.kt
@@ -1,12 +1,24 @@
 package com.lucky.fundiapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_signup.*
 
 class Signup : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        button_account_employer.setOnClickListener {
+            val intent = Intent(this, Employer_Signup::class.java)
+            startActivity(intent)
+        }
+
+        button_account_worker.setOnClickListener {
+            val intent = Intent(this, Worker_Signup::class.java)
+            startActivity(intent)
+        }
     }
 }
