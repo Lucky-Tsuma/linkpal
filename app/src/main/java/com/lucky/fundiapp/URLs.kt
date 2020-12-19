3ce89458485dd8615b5c37a678a9b3484a3edb5a
Lucky Tsuma <tsumahmumba@gmail.com>
2020-12-19 15:29:12 +0300

    Misc

--- app/src/main/java/com/lucky/fundiapp/URLs.kt
+++ app/src/main/java/com/lucky/fundiapp/URLs.kt
@@ -2,7 +2,7 @@ package com.lucky.fundiapp
 
 class URLs {
     companion object {
-        const val root_url = "http://192.168.43.209/Fundi_App"
+        private const val root_url = "http://192.168.43.209/Fundi_App"
         const val emp_register = "$root_url/register_employer.php"
         const val specialty_get = "$root_url/get_specialty.php"
         const val location_get = "$root_url/get_location.php"
