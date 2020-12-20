f2ab64e5ff13fd1a47a2fa65252a0f2358a27f05
Lucky Tsuma <tsumahmumba@gmail.com>
2020-12-20 17:46:54 +0300

    New register_worker url

--- app/src/main/java/com/lucky/fundiapp/URLs.kt
+++ app/src/main/java/com/lucky/fundiapp/URLs.kt
@@ -4,6 +4,7 @@ class URLs {
     companion object {
         private const val root_url = "http://192.168.43.209/Fundi_App"
         const val emp_register = "$root_url/register_employer.php"
+        const val worker_register = "$root_url/register_worker.php"
         const val specialty_get = "$root_url/get_specialty.php"
         const val location_get = "$root_url/get_location.php"
     }
