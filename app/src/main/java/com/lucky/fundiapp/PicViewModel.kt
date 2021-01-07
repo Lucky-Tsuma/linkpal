6abf3d7b40ae553a96f23d7a526163d9a88f8d28
Lucky Tsuma <tsumahmumba@gmail.com>
2021-01-07 13:18:14 +0300

    ViewModel class. Retain image after configuration change.

--- app/src/main/java/com/lucky/fundiapp/PicViewModel.kt
+++ app/src/main/java/com/lucky/fundiapp/PicViewModel.kt
@@ -1,4 +1,18 @@
 package com.lucky.fundiapp
 
-class PicViewModel {
+import android.graphics.drawable.Drawable
+import androidx.lifecycle.ViewModel
+
+/*ViewModel objects used to serve data to UI controllers(Fragments and Activities). In my case here, I use the PicViewModel class to create
+* ViewModel objects to retain an image and send it back to a UI component in case of configuration changes since, for memory intensive
+* data such as bitmaps, the Bundle? provided by onSaveInstanceState() will not suffice*/
+
+class PicViewModel : ViewModel(){
+    private var image : Drawable? = null
+
+    fun getImage(): Drawable? = image
+
+    fun setImage(image: Drawable) {
+        this.image = image
+    }
 }
