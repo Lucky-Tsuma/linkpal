044609c2bd40fa8f62e08a4016721271e5016acd
Lucky Tsuma <tsumahmumba@gmail.com>
2021-03-02 10:14:15 +0300

    While building a navigation drawer

---
+++ app/src/main/java/com/lucky/linkpal/EmployerHomeFragment.kt
@@ -0,0 +1,17 @@
+package com.lucky.linkpal
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class EmployerHomeFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_worker_home, container, false)
+    }
+}
