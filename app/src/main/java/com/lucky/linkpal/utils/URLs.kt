6eb08e9cb8d0fbc3ac306b5aac562d8d31dbc469
Lucky-Tsuma <tsumahmumba@gmail.com>
2021-07-17 15:21:27 +0300

    Misc

--- app/src/main/java/com/lucky/linkpal/utils/URLs.kt
+++ app/src/main/java/com/lucky/linkpal/utils/URLs.kt
@@ -2,7 +2,7 @@ package com.lucky.linkpal.utils
 
 class URLs {
     companion object {
-        const val root_url = "http://192.168.43.209/linkpal"
+        const val root_url = "http://192.168.1.186/linkpal"
         const val user_register = "$root_url/register_user.php"
         const val specialty_get = "$root_url/get_specialty.php"
         const val location_get = "$root_url/get_location.php"
