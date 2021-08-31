6267aae9e4ce19a8374704853f0b491538d981b7
Lucky-Tsuma <tsumahmumba@gmail.com>
2021-08-31 13:15:50 +0300

    New input validation functions

--- app/src/main/java/com/lucky/linkpal/Employer_Signup.kt
+++ app/src/main/java/com/lucky/linkpal/Employer_Signup.kt
@@ -1,7 +1,6 @@
 package com.lucky.linkpal
 
 import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -11,19 +10,33 @@ import com.lucky.linkpal.utils.SafeClickListener.Companion.setSafeOnClickListene
 import com.lucky.linkpal.utils.URLs
 import com.lucky.linkpal.utils.VolleyFileUploadRequest
 import kotlinx.android.synthetic.main.activity_employer__signup.*
+import kotlinx.android.synthetic.main.activity_login.*
 import org.json.JSONException
 import org.json.JSONObject
+import java.util.regex.Pattern
 
 
 class Employer_Signup : AppCompatActivity() {
-
-    private var status: Boolean = true
     private lateinit var firstname: String
     private lateinit var lastname: String
-    private lateinit var email: String
-    private lateinit var phone: String
+    private lateinit var longitude: String
+    private lateinit var latitude: String
+    private lateinit var phone_number: String
     private lateinit var password: String
     private lateinit var gender: String
+
+    private val PASSWORD_PATTERN =
+        Pattern.compile(
+            "^" +
+                    "(?=.*[0-9])" +
+                    "(?=.*[a-zA-Z])" +
+                    "(?=\\S+$)" +
+                    ".{6,}" +
+                    "$"
+        )
+
+    private val PHONE_PATTERN1 = Pattern.compile("^(07|01).*[0-9]")
+    private val PHONE_PATTERN2 = Pattern.compile("^(\\+254).*[0-9]")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,123 +44,106 @@ class Employer_Signup : AppCompatActivity() {
 
 
         button_sign_up_employer.setSafeOnClickListener {
-            checkUserInput()
-            if (status) {
-                registerEmployer()
+            if (!validateNames() || !validatePhoneNumber() || !validatePassword() || !validateGender()) {
+                return@setSafeOnClickListener
+            }
+            registerEmployer()
+        }
+    }
+
+    private fun validateNames(): Boolean {
+        firstname = employer_firstname.editText?.text.toString().trim()
+        lastname = employer_lastname.editText?.text.toString().trim()
+
+        employer_firstname.error = null
+        employer_lastname.error = null
+
+        return when {
+            firstname.isEmpty() -> {
+                employer_firstname.error = "Cannot be empty"
+                false
+            }
+            lastname.isEmpty() -> {
+                employer_lastname.error = "Cannot be empty"
+                false
+            }
+            else -> {
+                true
             }
         }
     }
 
-    private fun checkUserInput() {
-        /*reset in case there were problems with previous input*/
-        status = true
-        employer_firstname.setHintTextColor(Color.parseColor("#737373"))
-        employer_lastname.setHintTextColor(Color.parseColor("#737373"))
-        employer_email.setHintTextColor(Color.parseColor("#737373"))
-        employer_email.setTextColor(Color.BLACK)
-        employer_phone.setHintTextColor(Color.parseColor("#737373"))
-        employer_phone.setTextColor(Color.BLACK)
-        employer_password.setHintTextColor(Color.parseColor("#737373"))
-        employer_password.setTextColor(Color.BLACK)
-        employer_confirm_password.setHintTextColor(Color.parseColor("#737373"))
-        employer_confirm_password.setTextColor(Color.BLACK)
+    private fun validatePhoneNumber(): Boolean {
+        phone_number = employer_phone.editText?.text.toString().trim()
+        employer_phone.error = null
 
-        /*Getting user input strings. Deleting whitespaces*/
-        firstname = employer_firstname.text.toString().trim()
-        lastname = employer_lastname.text.toString().trim()
-        email = employer_email.text.toString().trim()
-        phone = employer_phone.text.toString().trim()
-        password = employer_password.text.toString().trim()
-        val password2: String = employer_confirm_password.text.toString().trim()
-
-        /*check whether any of the fields is not filled*/
-        if (firstname.isEmpty() || lastname.isEmpty() || email.isEmpty() || phone.isEmpty() ||
-            password.isEmpty() || password2.isEmpty()
+        return if (phone_number.isEmpty()) {
+            employer_phone.error = "Cannot be empty"
+            false
+        } else if (!PHONE_PATTERN1.matcher(phone_number).matches() && !PHONE_PATTERN2.matcher(
+                phone_number
+            ).matches()
         ) {
+            employer_phone.error = "Invalid input"
+            false
+        } else if (PHONE_PATTERN1.matcher(phone_number).matches() && phone_number.length != 10) {
+            employer_phone.error = "Invalid input"
+            false
+        } else if (PHONE_PATTERN2.matcher(phone_number).matches() && phone_number.length != 13) {
+            employer_phone.error = "Invalid input"
+            false
+        } else {
+            true
+        }
+    }
+
+    private fun validatePassword(): Boolean {
+        password = employer_password.editText?.text.toString().trim()
+        val password2 = employer_confirm_password.editText?.text.toString().trim()
+
+        employer_password.error = null
+        employer_confirm_password.error = null
+
+        return if (password.isEmpty()) {
+            employer_password.error = "Cannot be empty"
+            false
+        } else if (!PASSWORD_PATTERN.matcher(password).matches()) {
+            employer_password.error = "Invalid password format"
             Toast.makeText(
                 applicationContext,
-                "Please fill the highlighted fields",
+                "Password should be at least 6 characters long.\nShould have  both digits and letters.",
                 Toast.LENGTH_SHORT
             ).show()
-            status = false
-            if (firstname.isEmpty()) {
-                employer_firstname.setHintTextColor(Color.RED)
-            }
-            if (lastname.isEmpty()) {
-                employer_lastname.setHintTextColor(Color.RED)
-            }
-            if (email.isEmpty()) {
-                employer_email.setHintTextColor(Color.RED)
-            }
-            if (phone.isEmpty()) {
-                employer_phone.setHintTextColor(Color.RED)
-            }
-            if (password.isEmpty()) {
-                employer_password.setHintTextColor(Color.RED)
-            }
-            if (password2.isEmpty()) {
-                employer_confirm_password.setHintTextColor(Color.RED)
-            }
+            false
+        } else if (password2.isEmpty()) {
+            employer_confirm_password.error = "Cannot be empty"
+            false
+        } else if (!PASSWORD_PATTERN.matcher(password2).matches()) {
+            employer_confirm_password.error = "Invalid password format"
+            Toast.makeText(
+                applicationContext,
+                "Password should be at least 6 characters long.\nShould have  both digits and letters.",
+                Toast.LENGTH_SHORT
+            ).show()
+            false
+        } else if (!(password.matches(Regex(password2)))) {
+            Toast.makeText(applicationContext, "Passwords do not match", Toast.LENGTH_SHORT).show()
+            false
+        } else {
+            true
         }
+    }
 
-        /*Check for gender*/
-        if (status) {
-            if (!(gender_male_emp.isChecked || gender_female_emp.isChecked)) {
-                Toast.makeText(applicationContext, "Please select gender", Toast.LENGTH_SHORT)
-                    .show()
-                status = false
-            } else {
-                if (gender_male_emp.isChecked) gender = "M" else gender = "F"
-            }
+    private fun validateGender(): Boolean {
+        return if (!(gender_male_emp.isChecked || gender_female_emp.isChecked)) {
+            Toast.makeText(applicationContext, "Please select gender", Toast.LENGTH_SHORT)
+                .show()
+            false
+        } else {
+            gender = if (gender_male_emp.isChecked) "M" else "F"
+            true
         }
-
-        /*Checking email format and length*/
-        if (status) {
-            if (!(email.matches("(.*)@(.*)\\.(.*)".toRegex())) || email.length < 10
-                || email.startsWith("@") || email.endsWith("@")
-            ) {
-                Toast.makeText(applicationContext, "Invalid email address", Toast.LENGTH_SHORT)
-                    .show()
-                employer_email.setTextColor(Color.RED)
-                status = false
-            }
-        }
-
-        /*Checking phone number format and length*/
-        if (status) {
-            if (phone.length != 10 || (!(phone.matches(Regex("07(.*)"))) && !(phone.matches(Regex("01(.*)"))))) {
-                Toast.makeText(applicationContext, "Phone number is invalid", Toast.LENGTH_SHORT)
-                    .show()
-                employer_phone.setTextColor(Color.RED)
-                status = false
-            }
-        }
-
-        /*Checking for password length*/
-        if (status) {
-            if (password.length < 6 || password2.length < 6) {
-                Toast.makeText(applicationContext, "Password too short", Toast.LENGTH_SHORT).show()
-                status = false
-                if (password.length < 6) {
-                    employer_password.setTextColor(Color.RED)
-                }
-                if (password2.length < 6) {
-                    employer_confirm_password.setTextColor(Color.RED)
-                }
-            }
-        }
-
-        /*Checking whether passwords match*/
-        if (status) {
-            if (!(password.matches(Regex(password2)))) {
-                Toast.makeText(applicationContext, "Passwords do not match", Toast.LENGTH_SHORT)
-                    .show()
-                status = false
-                employer_password.setTextColor(Color.RED)
-                employer_confirm_password.setTextColor(Color.RED)
-            }
-        }
-    }/*check_user_input() ends here*/
+    }
 
     /*SEND EMPLOYER DATA TO SERVER*/
     private fun registerEmployer() {
@@ -191,8 +187,9 @@ class Employer_Signup : AppCompatActivity() {
                 try {
                     emp["firstName"] = firstname
                     emp["lastName"] = lastname
-                    emp["email"] = email
-                    emp["phone"] = phone
+                    emp["longitude"] = longitude
+                    emp["latitude"] = latitude
+                    emp["phone"] = phone_number
                     emp["password"] = password
                     emp["gender"] = gender
                 } catch (e: Exception) {
