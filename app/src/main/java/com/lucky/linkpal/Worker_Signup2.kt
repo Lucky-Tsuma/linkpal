83333b1caa8204c36fcee40532093d8e0210cdc9
Lucky-Tsuma <tsumahmumba@gmail.com>
2021-09-05 13:03:49 +0300

    Added location to this activity

--- app/src/main/java/com/lucky/linkpal/Worker_Signup2.kt
+++ app/src/main/java/com/lucky/linkpal/Worker_Signup2.kt
@@ -1,27 +1,39 @@
 package com.lucky.linkpal
 
+import android.Manifest
 import android.app.Activity
 import android.app.Dialog
 import android.content.Intent
+import android.content.IntentSender
+import android.content.pm.PackageManager
 import android.database.Cursor
 import android.graphics.Color
+import android.location.Location
 import android.net.Uri
 import android.os.Bundle
+import android.os.Looper
 import android.provider.OpenableColumns
 import android.view.View
 import android.widget.LinearLayout
 import android.widget.SimpleAdapter
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProviders
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+import com.google.android.gms.tasks.Task
+import com.google.android.material.snackbar.Snackbar
 import com.lucky.linkpal.utils.FileDataPart
 import com.lucky.linkpal.utils.SafeClickListener.Companion.setSafeOnClickListener
 import com.lucky.linkpal.utils.URLs
@@ -44,6 +56,8 @@ class Worker_Signup2 : AppCompatActivity() {
     private lateinit var jsonQueue: RequestQueue
     private lateinit var profileDescription: String
     private lateinit var workerViewModel: WorkerSignup2ViewModel
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
 
     private lateinit var firstname: String
     private lateinit var lastname: String
@@ -51,9 +65,34 @@ class Worker_Signup2 : AppCompatActivity() {
     private lateinit var password: String
     private lateinit var gender: String
     private var userJobField0: Int? = null
-
     private var imageData: ByteArray? = null
     private var uri: Uri? = null
+
+    /*Updating location in the case that the device cannot access it. For whatever reason*/
+    private val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult?) {
+            locationResult ?: return
+            for (location in locationResult.locations) {
+                longitude = location.longitude
+                latitude = location.latitude
+            }
+        }
+    }
+
+    /*After the user has been prompted to switch location on, pick the response and work with whichever they chose*/
+    private val resolutionForResult =
+        registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) { activityResult ->
+            if (activityResult.resultCode == RESULT_OK) {
+                pickLocation()
+            } else {
+                Toast.makeText(
+                    applicationContext,
+                    "Registration will proceed with an unknown location.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                checkForImage()
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -72,11 +111,21 @@ class Worker_Signup2 : AppCompatActivity() {
         /*Creating a ViewModel instance to help retain data in case the system destroys this activity*/
         workerViewModel = ViewModelProviders.of(this).get(WorkerSignup2ViewModel::class.java)
 
+        /*Setting up a location request*/
+        locationRequest = LocationRequest.create().apply {
+            interval = 10000
+            fastestInterval = 5000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+
         /*ON PROFILE PICTURE*/
         profile_pic.setSafeOnClickListener {
             val picIntent = Intent(Intent.ACTION_PICK)
             picIntent.type = "image/*"
-            resultLauncher.launch(picIntent)
+            imageLauncher.launch(picIntent)
         }
 
         if (workerViewModel.getImage() != null) {
@@ -113,12 +162,13 @@ class Worker_Signup2 : AppCompatActivity() {
         button_sign_up_worker.setSafeOnClickListener {
             checkUserInput()
             if (status) {
-                checkForImage()
+                pickLocation()
             }
         }
     }/*onCreate method ends here*/
 
-    private var resultLauncher =
+    /*Launches the selected image to the imageView*/
+    private var imageLauncher =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             if (result.resultCode == Activity.RESULT_OK) {
                 val data: Intent? = result.data
@@ -202,7 +252,7 @@ class Worker_Signup2 : AppCompatActivity() {
 
         /*get user input Strings*/
         userJobField = job_field.text.toString()
-        profileDescription = profile_description.editText.toString().trim()
+        profileDescription = profile_description.editText?.text.toString().trim()
 
         /*check for an empty field*/
         if (status) {
@@ -307,10 +357,12 @@ class Worker_Signup2 : AppCompatActivity() {
                     worker["password"] = password
                     worker["gender"] = gender
                     worker["jobField"] = userJobField0.toString()
-                    worker["longitude"] = longitude.toString()
-                    worker["latitude"] = latitude.toString()
                     worker["profileSummary"] = profileDescription
                     worker["imageName"] = imageName
+                    if (longitude != null && latitude != null) {
+                        worker["longitude"] = longitude.toString()
+                        worker["latitude"] = latitude.toString()
+                    }
                 } catch (e: Exception) {
                     e.printStackTrace()
                 }
@@ -332,6 +384,102 @@ class Worker_Signup2 : AppCompatActivity() {
         val inputStream = contentResolver.openInputStream(uri)
         inputStream?.buffered()?.use {
             imageData = it.readBytes()
+        }
+    }
+
+    /*launches dialog to request app location permission*/
+    private var requestLocationPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                pickLocation()
+            } else {
+                Toast.makeText(
+                    applicationContext,
+                    "Registration will proceed with an unknown location.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                checkForImage()
+            }
+        }
+
+    /*Checks for 3 situations:
+    * 1. the permission is already granted
+    * 2. there is need to explain to the user why we need the permission before asking for it
+    * 3. we just need to request the permission*/
+    private fun requestLocationPermission() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                pickLocation()
+            }
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) -> {
+                val snackBar = Snackbar.make(
+                    layout_worker_signup2,
+                    "You need to enable location to get suggestions on nearby users",
+                    Snackbar.LENGTH_INDEFINITE
+                )
+                snackBar.setAction("Ok") { snackBar.dismiss() }
+                snackBar.show()
+                requestLocationPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+            else -> {
+                requestLocationPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+        }
+    }
+
+    private fun pickLocation() {
+//        GETTING CURRENT LOCATION SETTINGS
+        val builder = LocationSettingsRequest.Builder().addLocationRequest(locationRequest)
+        val client: SettingsClient = LocationServices.getSettingsClient(this)
+        val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
+
+        //All location settings are satisfied. The client can make location requests
+        task.addOnSuccessListener {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                requestLocationPermission()
+            } else {
+                fusedLocationClient.lastLocation
+                    .addOnSuccessListener { location: Location? ->
+                        if (location != null) {
+                            longitude = location.longitude
+                            latitude = location.latitude
+                        } else {
+                            fusedLocationClient.requestLocationUpdates(
+                                locationRequest,
+                                locationCallback,
+                                Looper.getMainLooper()
+                            )
+                        }
+                    }
+                checkForImage()
+            }
+        }
+
+        task.addOnFailureListener { exception ->
+            if (exception is ResolvableApiException) {
+                // Location settings are not satisfied, but this can be fixed
+                // by showing the user a dialog.
+                try {
+                    val intentSenderRequest =
+                        IntentSenderRequest.Builder(exception.resolution).build()
+                    resolutionForResult.launch(intentSenderRequest)
+                } catch (sendEx: IntentSender.SendIntentException) {
+                    // Ignore the error.
+                }
+            }
         }
     }
 }
