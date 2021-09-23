3c035c1123e2ad956be1a19139a553b9c787a134
Lucky-Tsuma <tsumahmumba@gmail.com>
2021-09-23 13:04:53 +0300

    git diff --base app/src/main/res/layout/activity_post__a__job.xml

--- app/src/main/java/com/lucky/linkpal/Post_A_Job.kt
+++ app/src/main/java/com/lucky/linkpal/Post_A_Job.kt
@@ -3,23 +3,30 @@ package com.lucky.linkpal
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.content.pm.PackageManager
 import android.graphics.Color
 import android.os.Bundle
+import android.telephony.SmsManager
 import android.view.View
 import android.widget.LinearLayout
 import android.widget.SimpleAdapter
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
+import com.google.android.material.snackbar.Snackbar
 import com.lucky.linkpal.utils.SafeClickListener.Companion.setSafeOnClickListener
 import com.lucky.linkpal.utils.URLs
 import com.lucky.linkpal.utils.VolleyFileUploadRequest
 import kotlinx.android.synthetic.main.activity_post__a__job.*
+import org.json.JSONArray
 import org.json.JSONException
 import org.json.JSONObject
 
@@ -29,6 +36,8 @@ class Post_A_Job : AppCompatActivity() {
     private lateinit var job_type: String
     private lateinit var job_description: String
     private lateinit var jsonQueue: RequestQueue
+    private lateinit var phone_numbers: JSONArray
+    private var smsSent = false
     private var job_type_0: Int? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -60,7 +69,10 @@ class Post_A_Job : AppCompatActivity() {
         button_post.setSafeOnClickListener {
             checkUserInput()
             if (status) {
-                postJob()
+                getUserNumbers()
+                if (smsSent) {
+                    postJob()
+                }
             }
         }
 
@@ -157,6 +169,123 @@ class Post_A_Job : AppCompatActivity() {
                 status = false
                 editTxt_job_description.error =
                     "Description too short. should be at least 20 characters."
+            }
+        }
+    }
+
+    private fun getUserNumbers() {
+        val request = object : VolleyFileUploadRequest(Method.POST, URLs.user_numbers,
+            Response.Listener { response ->
+
+                val res = String(response.data)
+
+                try {
+                    val obj = JSONObject(res)
+                    val msg: String = obj.getString("message")
+
+                    if (msg == "true") {
+                        phone_numbers = obj.getJSONArray("phone_number")
+                        sendSms()
+                    } else {
+                        smsSent = true
+                    }
+                } catch (e: JSONException) {
+                    Toast.makeText(this, "Oops! An error occurred", Toast.LENGTH_SHORT).show()
+                }
+            },
+            Response.ErrorListener { error ->
+                if (error.toString().matches(Regex("(.*)NoConnectionError(.*)"))) {
+                    Toast.makeText(
+                        applicationContext,
+                        "Check your internet connection. Or try again later.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    Toast.makeText(applicationContext, error.toString(), Toast.LENGTH_SHORT).show()
+                }
+            }
+        ) {
+            override fun getParams(): MutableMap<String, String> {
+                val job = HashMap<String, String>()
+                try {
+                    job["job_id"] = job_type_0.toString()
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+                return job
+            }
+        }
+        /* request.retryPolicy = object : RetryPolicy {
+             override fun getCurrentTimeout(): Int {
+                 return 50000
+             }
+
+             override fun getCurrentRetryCount(): Int {
+                 return 50000
+             }
+
+             @Throws(VolleyError::class)
+             override fun retry(error: VolleyError) {
+             }
+         }*/
+        Volley.newRequestQueue(this).add(request)
+    }
+
+    private fun sendSms() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.SEND_SMS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestSmsPermission()
+        } else {
+            val smsManager = SmsManager.getDefault()
+            for (i in 0 until phone_numbers.length()) {
+                phone_numbers.getString(i)
+                smsManager.sendTextMessage(
+                    phone_numbers.getString(i),
+                    null,
+                    "A new job relevant to your profession has been posted on linkpal. Check it out.",
+                    null,
+                    null
+                )
+            }
+            smsSent = true
+        }
+    }
+
+    private var requestSmsPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                sendSms()
+            } else {
+                requestSmsPermission()
+            }
+        }
+
+    private fun requestSmsPermission() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.SEND_SMS
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                sendSms()
+            }
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                android.Manifest.permission.SEND_SMS
+            ) -> {
+                val snackBar = Snackbar.make(
+                    layout_post_a_job,
+                    "You need to enable sms permission to notify potential applicants.",
+                    Snackbar.LENGTH_INDEFINITE
+                )
+                snackBar.setAction("Ok") { snackBar.dismiss() }
+                snackBar.show()
+                requestSmsPermissionLauncher.launch(android.Manifest.permission.SEND_SMS)
+            }
+            else -> {
+                requestSmsPermissionLauncher.launch(android.Manifest.permission.SEND_SMS)
             }
         }
     }
